13c3503cc048989087ad854643ab20543694a9c5
kmikhailovskiy <konstantin.mikhailovskiy@postindustria.com>
2017-01-24 12:21:46 +0200

    Fixed AdMarketPlace service injection in order to make it parse XML in an appropriate way.

--- ZowdowDirectApiExample/app/src/main/java/com/zowdow/direct_api/injection/modules/NetworkModule.java
+++ ZowdowDirectApiExample/app/src/main/java/com/zowdow/direct_api/injection/modules/NetworkModule.java
@@ -15,6 +15,7 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 /**
  * Module which provides an access to network services.
@@ -33,32 +34,36 @@ public class NetworkModule {
     Retrofit.Builder provideRetrofitBuilder(OkHttpClient okHttpClient) {
         return new Retrofit.Builder()
                 .client(okHttpClient)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
+    }
+
+    @Provides
+    @Singleton
+    InitApiService provideInitApiService(Retrofit.Builder retrofitBuilder) {
+        return retrofitBuilder
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().serializeSpecialFloatingPointValues().create()))
+                .baseUrl(ApiBaseUrls.INIT_API).build()
+                .create(InitApiService.class);
+    }
+
+    @Provides
+    @Singleton
+    UnifiedApiService provideUnifiedApiService(Retrofit.Builder retrofitBuilder) {
+        return retrofitBuilder
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().serializeSpecialFloatingPointValues().create()))
+                .baseUrl(ApiBaseUrls.UNIFIED_API).build()
+                .create(UnifiedApiService.class);
+    }
+
+    @Provides
+    @Singleton
+    AdMarketPlaceService provideAdMarketPlaceService(OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .client(okHttpClient)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().serializeSpecialFloatingPointValues().create()));
-    }
-
-    @Provides
-    @Singleton
-    InitApiService provideInitApiService(Retrofit.Builder retrofitBuilder){
-        return retrofitBuilder.baseUrl(ApiBaseUrls.INIT_API).build().create(InitApiService.class);
-    }
-
-    @Provides
-    @Singleton
-    Retrofit provideUnifiedRetrofitAdapter(Retrofit.Builder retrofitBuilder) {
-        return retrofitBuilder.baseUrl(ApiBaseUrls.UNIFIED_API).build();
-    }
-
-    @Provides
-    @Singleton
-    UnifiedApiService provideUnifiedApiService(Retrofit retrofit) {
-        return retrofit.create(UnifiedApiService.class);
-    }
-
-    @Provides
-    @Singleton
-    AdMarketPlaceService provideAdMarketPlaceService(Retrofit retrofit) {
-        return retrofit.create(AdMarketPlaceService.class);
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .baseUrl(ApiBaseUrls.UNIFIED_API).build()
+                .create(AdMarketPlaceService.class);
     }
 
     @Provides
