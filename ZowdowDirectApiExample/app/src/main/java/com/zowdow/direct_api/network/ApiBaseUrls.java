b182d801aee0374d75cb75259a6fa1f28ab4dd59
Zowdow Admin <zadmin@zowdow.com>
2017-02-14 11:45:27 -0800

    Moved to its own repo.

--- ZowdowDirectApiExample/app/src/main/java/com/zowdow/direct_api/network/ApiBaseUrls.java
+++
@@ -1,6 +0,0 @@
-package com.zowdow.direct_api.network;
-
-public interface ApiBaseUrls {
-    String INIT_API = "https://i1.quick1y.com/v4/";
-    String UNIFIED_API = "https://u1.quick1y.com/v1/";
-}
